5d8155cd0e6c25b4db91ad250872a66f0787034c
Jay <imailasjay@gmail.com>
2026-04-24 01:13:24 +0100

    dont break my code.

--- src/crypto-infra/util/google-cloud-kms.ts
+++ src/crypto-infra/util/google-cloud-kms.ts
@@ -8,14 +8,14 @@ export default async function getDecryptedSeed() {
     process.env.GCP_PROJECT_ID!,
     'global',
     'my-seed-keyring',
-    'my-seed-key'
+    'my-seed-key',
   );
 
   // The encrypted blob you got from the gcloud command
   const ciphertext = Buffer.from(process.env.ENCRYPTED_SEED_BASE64!, 'base64');
 
   const [result] = await client.decrypt({ name, ciphertext });
-  
+
   // result.plaintext is a Buffer; convert to string to use in your crypto library
   return result.plaintext?.toString();
 }
