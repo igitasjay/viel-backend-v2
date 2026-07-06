2bff595555c97c44871c56501cd92b87a316ab45
Jay <imailasjay@gmail.com>
2026-07-06 03:46:17 +0100

    expose transaction status endpoint

--- src/monnify-infra/routes/monnify.route.ts
+++ src/monnify-infra/routes/monnify.route.ts
@@ -1,27 +1,30 @@
 import { Router } from 'express';
 import { getBankTransferDetails, initBankTransfer, queryTransactionStatus } from '@/controllers/monnify.controller';
+import { requireAuth } from '@/shared/middlewares';
 
-const router = Router();
+const monnifyRoutes = Router();
+
+monnifyRoutes.use(requireAuth)
 
 /**
  * @route GET /api/v1/monnify/transfer-details/:reference
  * @desc Fetch bank transfer details from Monnify
  * @access Public
  */
-router.get('/transfer-details/:reference', getBankTransferDetails);
+monnifyRoutes.get('/transfer-details/:reference', getBankTransferDetails);
 
 /**
  * @route GET /api/v1/monnify/status/:reference
  * @desc Query transaction status (supports ?type=payment or ?type=transaction)
  * @access Public
  */
-router.get('/status/:reference', queryTransactionStatus);
+monnifyRoutes.get('/status/:reference', queryTransactionStatus);
 
 /**
  * @route POST /api/v1/monnify/init-bank-transfer
  * @desc Initialize a bank transfer and get dynamic account details
  * @access Public
  */
-router.post('/init-bank-transfer', initBankTransfer);
+monnifyRoutes.post('/init-bank-transfer', initBankTransfer);
 
-export default router;
+export default monnifyRoutes;
--- src/routes/v1/routes.v2.ts
+++ src/routes/v1/routes.v2.ts
@@ -4,12 +4,15 @@ import { exchangeRateRoutes as adminExchangeRateRoutes } from '@/admins/routes/e
 import express from 'express'
 import { authRoutes } from '@/internals/authentication/auth.route';
 import { handleMonnifyWebhook } from '@/controllers/monnify.webhook';
+import monnifyRoutes from '@/monnify-infra/routes/monnify.route';
 const v2router = express.Router()
 
 v2router.use('/auth', authRoutes)
 v2router.use('/giftcards', giftcardroutes);
+v2router.use('/monnify', monnifyRoutes);
+v2router.post('/monnify/webhook', handleMonnifyWebhook);
+
 v2router.use('/admin/giftcards', adminGiftcardRoutes)
 v2router.use('/admin/exchange-rates', adminExchangeRateRoutes)
-v2router.post('/monnify/webhook', handleMonnifyWebhook);
 
 export default v2router
