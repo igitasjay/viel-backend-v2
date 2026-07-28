e125a8a184963d1dd04d0b1f65135d31a63f4526
Jay <imailasjay@gmail.com>
2026-07-28 10:59:08 +0100

    FIX: log obiex error for receiving crypto deposit.

--- src/internals/crypto/crypto.controller.ts
+++ src/internals/crypto/crypto.controller.ts
@@ -257,7 +257,7 @@ const webhookHandler = Asyncly(async (req: Request, res: Response) => {
     logger.info("✅ Obiex Event successfully processed");
     return res.status(200).json({ ok: true, message: "Event processed" });
   } catch (err) {
-    logger.error("❌ Webhook processing failed:");
+    logger.error(`❌ Webhook processing failed: ${err}`);
     return res.status(500).json({ error: "Failed to process webhook" });
   }
 });
