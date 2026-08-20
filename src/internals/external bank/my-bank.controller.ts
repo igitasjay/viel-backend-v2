31bb043da7fd95f13a2d26f04fa8cae585ded3a2
Jay <imailasjay@gmail.com>
2026-08-20 11:29:18 +0100

    FIX: fetching obiex bank codes from database for obiex fiat withdrawal

--- src/internals/external bank/my-bank.controller.ts
+++ src/internals/external bank/my-bank.controller.ts
@@ -44,7 +44,8 @@ const getCurrentUserBank = async (
         accountNumber: bankAccount.accountNumber,
         accountName: bankAccount.accountName,
         bankName: bankAccount.bankName,
-        bankCode: bankAccount.monnifyBankCode,
+        monnifyBankCode: bankAccount.monnifyBankCode,
+        obiexBankCode: bankAccount.obiexBankCode,
       },
     });
   } catch (error) {
